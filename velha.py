72813021f92dcc251cb5dab3cf3d7078ee549a5c
MarcosFajoli <markosalmeidaa@gmail.com>
2023-04-27 01:31:58 -0300

    Adding user input verification in tictactoe

--- velha.py
+++ velha.py
@@ -41,10 +41,26 @@ def jogar():
     print(f"3. Após {total_partidas} partidas, veremos quem é o verdadeiro vencedor...\n\nBoa sorte!\n")
 
     while continuar:
-        print("Iniciando o jogo.")
+        print("Iniciando o jogo. \n")
 
         marcacoes = resetar_marcacoes()
         print(tabuleiro(marcacoes))
+
+        entrada1_str: str = input("Jogador 1, escolha a posição a ser jogada no formato: 1, 1 (linha e coluna)\n")
+        entrada1_list: list = entrada1_str.split(", ")
+
+        # realiza a validação do número digitado
+        while len(entrada1_list) == 1 or \
+                len(entrada1_list) > 2 or \
+                not (entrada1_list[0].isnumeric() and entrada1_list[1].isnumeric()) or \
+                len(entrada1_list) == 1 or \
+                len(entrada1_list) > 2 or \
+                int(entrada1_list[0]) < 1 or \
+                int(entrada1_list[0]) > 3 or \
+                int(entrada1_list[1]) < 1 or \
+                int(entrada1_list[1]) > 3:
+            entrada1_str = input("Escolha um formato válido de posição: 1, 1 (até 3)\n")
+            entrada1_list = entrada1_str.split(", ")
 
         input_continuar: str = input("Deseja continuar a jogar? (Digite 1 para sim): ")
         if input_continuar != "1":
